34387ac3328d00e2ac7b87bd205ee86b8ae6410f
GaHyeon100 <goginala@g.skku.edu>
2025-02-26 04:15:28 +0900

    feat : 홈페이지 상단 및 body엔 상품 리스트 구성

--- lib/src/home/page/home_page.dart
+++ lib/src/home/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:bamtol_market_app/src/common/components/app_font.dart';
 import 'package:bamtol_market_app/src/common/controller/authentication_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,12 +10,93 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: GestureDetector(
-              onTap: () {
-                Get.find<AuthenticationController>().logout();
-              },
-              child: const AppFont('홈'))),
+      appBar: AppBar(
+          leadingWidth: Get.width * 0.6,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              children: [
+                const AppFont('아라동',
+                    fontWeight: FontWeight.bold, size: 20, color: Colors.white),
+                const SizedBox(
+                  width: 10,
+                ),
+                SvgPicture.asset('assets/svg/icons/bottom_arrow.svg'),
+              ],
+            ),
+          ),
+          actions: [
+            SvgPicture.asset('assets/svg/icons/search.svg'),
+            const SizedBox(width: 15),
+            SvgPicture.asset('assets/svg/icons/list.svg'),
+            const SizedBox(width: 15),
+            SvgPicture.asset('assets/svg/icons/bell.svg'),
+            const SizedBox(width: 25)
+          ]),
+      body: const _ProductList(),
     );
   }
+}
+
+class _ProductList extends StatelessWidget {
+  const _ProductList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+        padding: const EdgeInsets.only(left: 25.0, top: 20, right: 25),
+        itemBuilder: (context, index) {
+          return _productOne(index);
+        },
+        separatorBuilder: (context, index) => const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Divider(color: Color(0xff3C3C3E))),
+        itemCount: 10);
+  }
+}
+
+Widget _productOne(int index) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      ClipRRect(
+        borderRadius: BorderRadius.circular(7),
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: Image.network(
+            'http://cdn.kgmaeil.net/news/photo/202007/245825_49825_2217.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+      const SizedBox(width: 15),
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          AppFont(
+            '상품 $index 무료로 드려요 :)',
+            color: Colors.white,
+            size: 16,
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          const AppFont('개발자 2025.02.26.', color: Color(0xff878B93), size: 12),
+          const SizedBox(
+            height: 5,
+          ),
+          const AppFont(
+            '나눔',
+            size: 14,
+            fontWeight: FontWeight.bold,
+          )
+        ],
+      )),
+    ],
+  );
 }
